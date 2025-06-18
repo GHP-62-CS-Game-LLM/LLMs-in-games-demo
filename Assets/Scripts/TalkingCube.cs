8116ed26a88108e92f4401fefcbd0202174676f0
Nathan Q <Natethegreat08q@gmail.com>
2025-06-18 12:08:01 -0400

    add ProBuilder and change floor

--- Assets/Scripts/TalkingCube.cs
+++ Assets/Scripts/TalkingCube.cs
@@ -1,5 +1,6 @@
-using TMPro.EditorUtilities;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class TalkingCube : MonoBehaviour
 {
@@ -12,10 +13,15 @@ public class TalkingCube : MonoBehaviour
         Debug.Log("Starting Convo");
         _conversation = manager.MakeConversation();
 
+        Stopwatch stopwatch = new Stopwatch();
+    
+        stopwatch.Start();
         await _conversation.Message("Hello, I am a talking cube!");
         await _conversation.Message("I am a 1x1x1m cube floating in space");
         await _conversation.Message("What are you?");
+        stopwatch.Stop();
         
         Debug.Log($"Conversation: {_conversation}");
+        Debug.Log($"Elapsed Time: {stopwatch.Elapsed}");
     }
 }
